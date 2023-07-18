165bd6f2912754a2a8991b68036bc87b97d9a0af
Hillel1408 <etemax@bk.ru>
2023-07-18 12:00:13 +0300

    drivingschoolvl

--- #src/js/script.js
+++ #src/js/script.js
@@ -14,4 +14,14 @@ const swiper2 = new Swiper('.video-reviews__swiper', {
 new CircleType(document.getElementById('demo3'));
 new CircleType(document.getElementById('demo4'));
 
-const player = new Plyr('#player');
+const play = document.querySelectorAll('.play');
+
+play.forEach((item) => {
+    item.addEventListener('click', function () {
+        this.parentElement.childNodes[1].src += '&autoplay=1';
+        setTimeout(() => {
+            this.parentElement.childNodes[3].style.display = 'none';
+            this.style.display = 'none';
+        }, 700);
+    });
+});
